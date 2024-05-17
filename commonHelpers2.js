856bded447c0e9642d1882af8b9aeb26631486aa
anastasiia-riabokon <161604137+anastasiia-riabokon@users.noreply.github.com>
2024-05-17 08:46:09 +0000

    Deploying to gh-pages from @ anastasiia-riabokon/goit-js-hw-10@fb23cd2e8e1ba9a3abe65a6f0886a1e2fdf73def 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const t=document.querySelectorAll(".radio-field"),d=document.querySelector("fieldset");t.forEach(e=>{e.addEventListener("change",()=>{d.classList.add("radio-selected")})});
+//# sourceMappingURL=commonHelpers2.js.map
