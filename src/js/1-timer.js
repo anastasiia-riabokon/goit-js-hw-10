1c5ab66e49ac9e33574fb9d0f1c01034e13b0450
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-17 10:40:31 +0300

    'upd'

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -46,7 +46,7 @@ const options = {
         backgroundColor: '#EF4040',
         titleColor: '#FFF',
         messageColor: '#FFF',
-        iconUrl: '/img/icon-error.svg',
+        iconUrl: './img/icon-error.svg',
       });
       btnStart.disabled = true;
     } else {
